044b5df6440bd9e94e412c54731d74407f8f00cc
Romain ACCIARI <romain.acciari@gmail.com>
2015-07-31 14:07:51 +0200

    Fix puppet definitions with latest openiosds module

--- manifests/oioswift-1.pp
+++
@@ -1,6 +0,0 @@
-openiosds::oioswift {'oioswift-1':
-  num => '1',
-  ns => 'OPENIO',
-  ipaddress => '0.0.0.0',
-  sds_proxy_url => 'http://VAGRANT_MAIN_VM:6012',
-}
--- manifests/vagrant-ext-swift.pp
+++ manifests/vagrant-ext-swift.pp
@@ -57,3 +57,9 @@ keystone_user_role { 'demo@demo':
   roles  => ['admin','_member_'],
   ensure => present
 }
+openiosds::oioswift {'oioswift-1':
+  num => '1',
+  ns => 'OPENIO',
+  ipaddress => '0.0.0.0',
+  sds_proxy_url => 'http://VAGRANT_MAIN_VM:6012',
+}
